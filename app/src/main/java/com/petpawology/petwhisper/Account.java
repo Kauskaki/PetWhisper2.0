e6e0e081a4a9361f98de4bad4266547abb72b08d
Menua Melikyan <melikyan.business@gmail.com>
2025-05-09 02:11:27 -0700

    friend pets

--- app/src/main/java/com/petpawology/petwhisper/Account.java
+++ app/src/main/java/com/petpawology/petwhisper/Account.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Account {
     List<Friend> friends = new ArrayList<>();
 
+
     public Account(){
         friends.add(new Friend("Alice", Arrays.asList(new Pet("Buddy"), new Pet("Mittens"))));
         friends.add(new Friend("Bob", Arrays.asList(new Pet("Rex"), new Pet("Whiskers"))));
